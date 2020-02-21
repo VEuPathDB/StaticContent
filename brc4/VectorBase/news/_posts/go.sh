b7f217f18ad794f46fd26f8e43e4de766e5ddfd3
aurreco <aurreco@yahoo.com>
2020-02-20 23:37:51 -0500

    reading common and specific cards

--- brc4/VectorBase/news/_posts/go.sh
+++ brc4/VectorBase/news/_posts/go.sh
@@ -1,6 +1,6 @@
-# change all the "*.html" files in the current directory
+# change all the "*.md" files in the current directory
 
-for i in `ls *html`
+for i in `ls *md`
 do
   echo "working on $i ..."
   sed -f changes.sed < $i > ${i}.tmp
