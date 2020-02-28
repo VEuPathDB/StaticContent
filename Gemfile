b4695b06590f031b6cedf3a0e804e0040661d209
Elizabeth Harper <epharper@upenn.edu>
2020-02-28 11:38:42 -0500

    removed jekyll_pages_api plugin

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source "https://rubygems.org"
 gem "jekyll-seo-tag"
 
 group :jekyll_plugins do
-    gem 'jekyll_pages_api'
     gem 'jekyll-feed'
 end
---
+++ _plugins/site-search.rb
@@ -0,0 +1,39 @@
+require 'rbconfig'
+require 'json'
+
+def os
+  @os ||= (
+  host_os = RbConfig::CONFIG['host_os']
+  case host_os
+  when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+    :".exe"
+  when /darwin|mac os/
+    :"-darwin"
+  when /linux/
+    :"-linux"
+  else
+    raise Error::WebDriverError, "Unsupported os: #{host_os.inspect}"
+  end
+  )
+end
+
+Jekyll::Hooks.register :site, :post_render do |site|
+  puts "       Site Search: Generating site search json"
+  cmd = "_scripts/post-process#{os}"
+
+  IO.popen(cmd, "r+") do |pipe|
+    site.pages.each do |page|
+      if page.ext != '.json'
+        tmp = JSON.generate({:header => page.data, :output => page.output})
+        pipe << "#{tmp}\n"
+      end
+    end
+    pipe.close_write
+    puts pipe.read
+  end
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  `rm -rf _site/api`
+  `mv -t _site api`
+end
